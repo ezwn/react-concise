554a958fadc3611034eb4714c074b0d843844d94
Nicolas Enzweiler <nicolas.ezw.itdev@gmail.com>
2023-11-01 08:16:29 +0100

    feat(useValueChanged): a hook to check if a value changed between renders

--- src/usePreviousValue.ts
+++ src/usePreviousValue.ts
@@ -1,10 +1,25 @@
 import { useEffect, useRef } from "react";
 
-export const usePreviousValue = <T>(props: T) => {
+/**
+ * Stores the value for the current render and provides the one from the previous one.
+ * 
+ * @param value anything
+ * @returns 
+ */
+export const usePreviousValue = <T>(value: T) => {
     const ref = useRef<T>();
     useEffect(() => {
-        ref.current = props;
+        ref.current = value;
     });
     return ref.current;
 }
 
+/**
+ * Will return true if the value changed between the previous render and the current one.
+ * 
+ * @param value anything
+ * @returns 
+ */
+export const useValueChanged = <T>(currValue: T) => {
+    return usePreviousValue(currValue) !== currValue;
+}
